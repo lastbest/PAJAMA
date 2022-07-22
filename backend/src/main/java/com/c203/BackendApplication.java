cc8ae67ff871918e57c507ee74578d711e452f25
김유완 <opal18@naver.com>
2022-07-22 11:33:17 +0900

    [ETC] 코드 수정 Letter.java fetch 추가, 코드 주석 삭제

--- backend/src/main/java/com/c203/BackendApplication.java
+++ backend/src/main/java/com/c203/BackendApplication.java
@@ -14,20 +14,7 @@ import javax.persistence.Persistence;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("practice" );
-//		EntityManager em = emf.createEntityManager();
-//		EntityTransaction tx = em.getTransaction();
-//		tx.begin();
-//		try{
-//			User user = new User();
-//			user.setUser_id("테스트");
-//			tx.commit();
-//		}catch (Exception e){
-//			tx.rollback();
-//		}finally {
-//			em.close();
-//		}
-//		emf.close();
+
 	SpringApplication.run(BackendApplication.class, args);
 	}
 
