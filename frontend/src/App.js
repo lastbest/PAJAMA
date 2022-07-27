1a1c23389ce63838ab88ddd1d337712e155a3c8f
이동주 <joey0319@naver.com>
2022-07-27 10:34:07 +0900

    [STYLE] HomePage 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import RoomPage from './pages/RoomPage';
+import MyPage from './pages/MyPage';
+import UpdateUserPage from './pages/UpdateUserPage';
+import FeedPage from './pages/FeedPage';
+import CreatePartyPage from './pages/CreatePartyPage';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
         <Route element={<LoginPage/>} path='/login' />
         <Route element={<RegisterPage/>} path='/register' />
         <Route element={<RoomPage/>} path='/room'/>
+        <Route element={<MyPage/>} path='/mypage'/>
+        <Route element={<UpdateUserPage/>} path='/mypage/update'/>
+        <Route element={<FeedPage/>} path='/mypage/feed'/>
+        <Route element={<CreatePartyPage/>} path='/createparty'/>
       </Routes>
     </>
     
