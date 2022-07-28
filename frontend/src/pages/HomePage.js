2ed56cd234f5731ba0015691eda7141d90c748c3
jh <jinju0405@naver.com>
2022-07-28 16:37:29 +0900

    [STYLE] HomePage Button 클릭시 Modal 창 수정

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../components/nav/NavBar';
 import HomeCarousel from '../components/homecarousel/HomeCarousel';
 import styled from 'styled-components';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 const CreateBtn = styled.div`
     display: flex;
@@ -27,13 +30,53 @@ const StyledBtn = styled.button`
 `
 
 const HomePage = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    let token = sessionStorage.getItem('accessToken')
+
     return (
         <div>
             <NavBar></NavBar>
             <HomeCarousel></HomeCarousel>
-            <CreateBtn>
+            {/* <CreateBtn>
                 <a href='/createparty'><StyledBtn>START PARTY</StyledBtn></a>
+            </CreateBtn> */}
+            <CreateBtn>
+                <StyledBtn variant="primary" onClick={
+                    ()=>{
+                        if(!token){
+                            handleShow()
+                        } else {
+                            document.location.href="/createparty"
+                        }
+                    }
+                    }>
+                    CREATE PARTY
+                </StyledBtn>
             </CreateBtn>
+
+            <Modal
+                style={{'top':'200px'}}
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                <Modal.Title style={{'font-family':'star'}}>PAZAMA</Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{'font-family':'oldpicture', 'font-size':'20px'}}>
+                로그인이 필요한 서비스입니다.
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant='secondary' style={{'border':'none','font-family':'oldpicture'}} onClick={handleClose}>
+                    Close
+                </Button>
+                <Button style={{'color':'black', 'backgroundColor':'#FD7A99', 'border':'none','font-family':'oldpicture', 'box-shadow':'none' }} onClick={()=>{document.location.href='/login'}}>로그인</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 };
