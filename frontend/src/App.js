f9ec2194919a9cb2f76f04f49fd95848359b5da8
Jaecheol Jeong <paneko3@gmail.com>
2022-07-29 14:50:20 +0900

    [FEAT][A-06][A-07][A-08][A-09][A-10]:이메일인증및아이디,비밀번호찾기구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,32 @@
-import {Routes, Route } from 'react-router-dom';
-import './App.css';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import RoomPage from './pages/RoomPage';
-import MyPage from './pages/MyPage';
-import UpdateUserPage from './pages/UpdateUserPage';
-import FeedPage from './pages/FeedPage';
-import CreatePartyPage from './pages/CreatePartyPage';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import RoomPage from "./pages/RoomPage";
+import MyPage from "./pages/MyPage";
+import UpdateUserPage from "./pages/UpdateUserPage";
+import FeedPage from "./pages/FeedPage";
+import CreatePartyPage from "./pages/CreatePartyPage";
+import FindIdPage from "./pages/FindIdPage";
+import FindpwdPage from "./pages/FindpwdPage";
 
 function App() {
   return (
-
     <>
       <Routes>
-        <Route element={<HomePage/>} path='/' />
-        <Route element={<LoginPage/>} path='/login' />
-        <Route element={<RegisterPage/>} path='/register' />
-        <Route element={<RoomPage/>} path='/room'/>
-        <Route element={<MyPage/>} path='/mypage'/>
-        <Route element={<UpdateUserPage/>} path='/mypage/update'/>
-        <Route element={<FeedPage/>} path='/mypage/feed'/>
-        <Route element={<CreatePartyPage/>} path='/createparty'/>
+        <Route element={<HomePage />} path="/" />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<FindIdPage />} path="/findId" />
+        <Route element={<FindpwdPage />} path="/findPwd" />
+        <Route element={<RegisterPage />} path="/register" />
+        <Route element={<RoomPage />} path="/room" />
+        <Route element={<MyPage />} path="/mypage" />
+        <Route element={<UpdateUserPage />} path="/mypage/update" />
+        <Route element={<FeedPage />} path="/mypage/feed" />
+        <Route element={<CreatePartyPage />} path="/createparty" />
       </Routes>
     </>
-    
   );
 }
 
--- frontend/src/components/auth/AuthRegisterForm.js
+++ frontend/src/components/auth/AuthRegisterForm.js
@@ -9,7 +9,7 @@ const AuthFormBlock = styled.div`
     margin: 0;
     color: black;
     margin-bottom: 1rem;
-    font-family:"star";
+    font-family: "star";
   }
 `;
 
@@ -39,22 +39,22 @@ const ButtonWithMarinTop = styled(Button)`
 `;
 
 const StyleButton = styled.button`
-  margin-top:4px;
+  margin-top: 4px;
   border: none;
   border-radius: 10px;
   font-size: 0.8rem;
-  font-family:"oldpicture";
+  font-family: "oldpicture";
   height: 2rem;
   color: black;
   outline: none;
   cursor: pointer;
   display: inline;
   margin-left: -70px;
-  background: #FD7A99;
+  background: #fd7a99;
   &:hover {
-    background: #FFA4BD;
+    background: #ffa4bd;
   }
-`
+`;
 
 const AuthRegisterForm = () => {
   let [credentials, setCredentials] = useState({
@@ -62,8 +62,21 @@ const AuthRegisterForm = () => {
     tel: "",
     pwd: "",
     nickname: "",
-    name: ""
+    name: "",
   });
+
+  let [check, setCheck] = useState({
+    id: "",
+    type: "",
+  });
+
+  let [credential, setCredential] = useState({
+    authNumber: "",
+    email: "",
+  });
+  const [emailvalid, setEmailvalid] = useState(false);
+  const [count, setCount] = useState(0);
+
   let [password1, setPassword1] = useState("");
   let [password2, setPassword2] = useState("");
   let [tel, setTel] = useState("");
@@ -73,55 +86,61 @@ const AuthRegisterForm = () => {
   let [code, setCode] = useState("");
   function onSubmit(e) {
     e.preventDefault();
-    if (password2 === password1) {
-      if (password1.length < 6 || password1.length > 16) {
-        alert("비밀번호는 6~16자리로 설정해야 합니다.");
-      } else {
-        let countNum = 0;
-        let countEng = 0;
-        let i = 0;
-        for (i = 0; i < password1.length; i++) {
-          if (password1.charAt(i) >= "A" && password1.charAt(i) <= "z") {
-            countEng++;
-          } else if (password1.charAt(i) >= "0" && password1.charAt(i) <= "9") {
-            countNum++;
+    if (emailvalid) {
+      if (password2 === password1) {
+        if (password1.length < 6 || password1.length > 16) {
+          alert("비밀번호는 6~16자리로 설정해야 합니다.");
+        } else {
+          let countNum = 0;
+          let countEng = 0;
+          let i = 0;
+          for (i = 0; i < password1.length; i++) {
+            if (password1.charAt(i) >= "A" && password1.charAt(i) <= "z") {
+              countEng++;
+            } else if (
+              password1.charAt(i) >= "0" &&
+              password1.charAt(i) <= "9"
+            ) {
+              countNum++;
+            }
+          }
+          if (countEng === 0 || countNum === 0) {
+            alert("비밀번호는 숫자와 영문자를 혼용하여야 합니다.");
+          } else {
+            setCredentials(
+              (credentials.email = userEmail),
+              (credentials.pwd = password1),
+              (credentials.name = userName),
+              (credentials.nickname = userNickname),
+              (credentials.tel = tel)
+            );
+            axios({
+              url: "http://localhost:8080/users",
+              method: "post",
+              data: credentials,
+            })
+              .then((res) => {
+                if (res.data.result === "success") {
+                  window.alert("회원가입 성공");
+                  document.location.href = "/";
+                } else {
+                  window.alert("이미 존재하는 이메일입니다.");
+                  document.location.href = "/register";
+                }
+              })
+              .catch(() => {
+                console.log("회원가입 실패");
+                alert("회원가입 실패");
+                document.location.href = "/register";
+              });
           }
         }
-        if (countEng === 0 || countNum === 0) {
-          alert("비밀번호는 숫자와 영문자를 혼용하여야 합니다.");
-        } else {
-          setCredentials(
-            (credentials.email = userEmail),
-            (credentials.pwd = password1),
-            (credentials.name = userName),
-            (credentials.nickname = userNickname),
-            (credentials.tel = tel)
-          );
-          axios({
-            url: "http://localhost:8080/users",
-            method: "post",
-            data: credentials
-          })
-            .then((res) => {
-              if (res.data.result === 'success') {
-                window.alert('회원가입 성공')
-                document.location.href="/"
-              } else {
-                window.alert('이미 존재하는 이메일입니다.')
-                document.location.href="/register"
-
-              }
-            })
-            .catch(() => {
-              console.log("회원가입 실패");
-              alert('회원가입 실패')
-              document.location.href="/register"
-            });
-        }
+      } else {
+        alert("비밀번호가 일치하지 않습니다.");
+        document.location.href = "/register";
       }
     } else {
-      alert("비밀번호가 일치하지 않습니다.");
-      document.location.href="/register"
+      alert("이메일 인증을 완료하십시오.");
     }
   }
   return (
@@ -133,18 +152,35 @@ const AuthRegisterForm = () => {
             autoComplete="userEmail"
             name="userEmail"
             placeholder=" 이메일"
-            type="email"
             onInput={(event) => {
               setUserEmail(event.target.value);
             }}
             required
           />
-          <StyleButton onClick={()=>{
-            axios({
-              url:"http://localhost:8080/users/mail",
-              mehotd:'post'
-            })
-          }}>인증하기</StyleButton>
+
+          <StyleButton
+            onClick={(e) => {
+              e.preventDefault();
+              setCount(count + 1);
+              setCheck((check.id = userEmail), (check.type = "0"));
+              console.log(check);
+              console.log("시작");
+              axios({
+                url: "http://localhost:8080/users/mail",
+                method: "post",
+                data: check,
+              })
+                .then((res) => {
+                  console.log(res);
+                  console.log("성공");
+                })
+                .catch((res) => {
+                  console.log(res);
+                });
+            }}
+          >
+            {count === 0 ? "인증하기" : "재인증"}
+          </StyleButton>
         </div>
         <div className="d-flex">
           <StyledInput
@@ -153,22 +189,53 @@ const AuthRegisterForm = () => {
             placeholder=" 이메일 인증번호"
             type="emailcheck"
             onInput={(event) => {
+              event.preventDefault();
               setCode(event.target.value);
             }}
             required
           />
-          <StyleButton onClick={()=>{
-            axios({
-              url:"http://localhost:8080/users/mail",
-              method:'get',
-              data: code
-            })
-            .then((res)=>{
-              if (res.data.result === 'success') {
-                
-              }
-            })
-          }}>확인하기</StyleButton>
+
+          {emailvalid ? (
+            <StyleButton
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              인증완료
+            </StyleButton>
+          ) : (
+            <StyleButton
+              onClick={() => {
+                setCredential(
+                  (credential.authNumber = code),
+                  (credential.email = userEmail)
+                );
+                console.log(credential);
+                console.log("확인시작");
+
+                axios({
+                  url: "http://localhost:8080/users/mail",
+                  method: "get",
+                  params: credential,
+                })
+                  .then((res) => {
+                    console.log(res);
+                    console.log("성공");
+                    if (res.data.result === "success") {
+                      setEmailvalid(true);
+                    } else {
+                      alert("인증번호가 유효하지 않습니다.");
+                    }
+                  })
+                  .catch((res) => {
+                    console.log(res);
+                    alert("인증");
+                  });
+              }}
+            >
+              확인하기
+            </StyleButton>
+          )}
         </div>
         <StyledInput
           autoComplete="current-password"
@@ -217,11 +284,7 @@ const AuthRegisterForm = () => {
           }}
           required
         />
-        <ButtonWithMarinTop
-          fullWidth
-        >
-          회원가입
-        </ButtonWithMarinTop>
+        <ButtonWithMarinTop fullWidth>회원가입</ButtonWithMarinTop>
       </form>
     </AuthFormBlock>
   );
