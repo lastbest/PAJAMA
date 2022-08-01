25052e844901b096652311bc1627aac506d5fb8d
DESKTOP-KVCQHCD\multicampus <paneko3@gmail.com>
2022-08-01 13:29:12 +0900

    [FEAT][B-01]파티룸임시구현

--- frontend/src/pages/RoomPage.js
+++ frontend/src/pages/RoomPage.js
@@ -1,47 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
+import Counter from "../components/common/Counter";
+import styles from "./RoomPage.module.css";
+import Button from "../components/common/Button";
 
 const Header = styled.div`
-    display: grid;
-    grid-template-columns: 10% 10% 10% 10%;
-    justify-content: space-around;
-    text-align: center;
-    background-color: #FFE9EF;
-    img {
-        margin-top: 10px;
-    }
-    height: 70px;
-    padding: 3px;
-    
+  display: grid;
+  grid-template-columns: 10% 10% 10% 10%;
+  justify-content: space-around;
+  text-align: center;
+  background-color: #ffe9ef;
+  img {
+    margin-top: 10px;
+  }
+  height: 70px;
+  padding: 3px;
 `;
 
 const StyledBtn = styled.button`
-    width: 100px;
-    height: 70px;
-    border: none;
-    border-radius: 10px;
-    font-size: 1rem;
-    font-weight: bold;
-    outline: none;
-    cursor: pointer;
-    background: none;
-    &:hover {
-        background: #FFA4BD;
-    }
-`
+  width: 100px;
+  height: 70px;
+  border: none;
+  border-radius: 10px;
+  font-size: 1rem;
+  font-weight: bold;
+  outline: none;
+  cursor: pointer;
+  background: none;
+  &:hover {
+    background: #ffa4bd;
+  }
+`;
 
 const RoomPage = () => {
-    return (
-        <>
-        <Header>
-            <img src='/pazamafont.png' alt='logo' width='100px' height='50px'></img>
-            <StyledBtn><img src='/love-letter.png' alt='letter' width='40px' height='40px'></img></StyledBtn>
-            <StyledBtn><img src='/birthday-cake.png' alt='cake' width='40px' height='40px'></img></StyledBtn>
-            <StyledBtn><img src='/camera.png' alt='camera' width='40px' height='40px'></img></StyledBtn>
-        </Header>
-        </>
-    )
+  return (
+    <div className={styles.container}>
+      <div>
+        <Counter className={styles.counter} />
+      </div>
+      <div>
+        <Button className={styles.button}>입장하기</Button>
+      </div>
+    </div>
+  );
 };
 
 export default RoomPage;
