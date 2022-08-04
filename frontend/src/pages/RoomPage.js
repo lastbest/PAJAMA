ff40bab5b4a880a6ff99806ab30bde62a0264bf0
DESKTOP-KVCQHCD\multicampus <paneko3@gmail.com>
2022-08-04 14:08:40 +0900

    [FEAT][C-05]채팅기능구현

--- frontend/src/pages/RoomPage.js
+++ frontend/src/pages/RoomPage.js
@@ -46,23 +46,23 @@ const RoomPage = () => {
     // alert("로그인이 필요한 서비스입니다.");
     // navigate("/login");
   } else {
-    setInterval(function () {
-      var today = new Date().getTime();
-      var gap = dday - today;
-      var day = Math.ceil(gap / (1000 * 60 * 60 * 24));
-      var hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
-      var sec = Math.ceil((gap % (1000 * 60)) / 1000);
-      document.getElementById("counter").innerHTML =
-        "D-" +
-        day.toString().padStart(2, "0") +
-        " : " +
-        hour.toString().padStart(2, "0") +
-        " : " +
-        min.toString().padStart(2, "0") +
-        " : " +
-        sec.toString().padStart(2, "0");
-    }, 1000);
+    // setInterval(function () {
+    //   var today = new Date().getTime();
+    //   var gap = dday - today;
+    //   var day = Math.ceil(gap / (1000 * 60 * 60 * 24));
+    //   var hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    //   var min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
+    //   var sec = Math.ceil((gap % (1000 * 60)) / 1000);
+    //   document.getElementById("counter").innerHTML =
+    //     "D-" +
+    //     day.toString().padStart(2, "0") +
+    //     " : " +
+    //     hour.toString().padStart(2, "0") +
+    //     " : " +
+    //     min.toString().padStart(2, "0") +
+    //     " : " +
+    //     sec.toString().padStart(2, "0");
+    // }, 1000);
   }
 
   const [show, setShow] = useState(false);
