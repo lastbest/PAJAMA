53690057e3ac0e44eacf54230e9ef9f11c806c42
진주현 <jinju0405@naver.com>
2022-08-12 16:39:52 +0900

    [REFACTOR] invite 페이지 코드 수정

--- frontend/src/pages/InvitePage.js
+++ frontend/src/pages/InvitePage.js
@@ -3,24 +3,32 @@ import { useParams } from "react-router-dom";
 import "./InvitePage.css";
 import axios from "axios";
 import { BrowserView, MobileView } from 'react-device-detect';
+import NavBar from "../components/nav/NavBar";
+
 
 const InvitePage = () => {
   let token = sessionStorage.getItem("accessToken");
-  let [nickname, setNickname] = useState("");
-  let [room_idx, setRoomidx] = useState("");
+  let [partyNickname, setPartyNickname] = useState("");
+  let [partyName, setPartyName] = useState("");
+  let [partyDesc, setPartyDesc] = useState("");
+  let [partyDate, setPartyDate] = useState("");
   let { roomIdx } = useParams();
 
-  // useEffect(() => {
-  //   axios({
-  //     url: "http://i7c203.p.ssafy.io:8082/lettters/{room_idx}",
-  //     method: "get",
-  //     params: { nickname: nickname, room_idx:room_idx },
-  //   })
-  //     .then((res) => {
-  //       setNickname(res.data.result.nickname);
-  //       setRoomidx(res.data.result.room_idx);
-  //     })
-  // }, []);
+  useEffect(() => {
+    axios({
+      url: "http://i7c203.p.ssafy.io:8082/rooms",
+      method: "get",
+      headers: { accessToken: token },
+      params: { roomIdx:roomIdx },
+    })
+      .then((res) => {
+        setPartyNickname(res.data.result.partyNickname);
+        setPartyName(res.data.result.partyName);
+        setPartyDesc(res.data.result.partyDesc);
+        setPartyDate(res.data.result.partyDate);
+      })
+  }, []);
+
 
   return (
     <>
@@ -34,11 +42,11 @@ const InvitePage = () => {
           height="60px"
         ></img>
       </a>
-      <p className="Main">{nickname}님의 파티에 초대받으셨습니다.</p>
+      <p className="Main">{partyNickname}님의 파티에 초대받으셨습니다.</p>
       <div className="partyinfo">
-        <p>파티제목</p>
-        <p>파티정보</p>
-        <p>파티날짜</p>
+        <p style={{color:"#FD7A99", fontSize:"large", fontWeight:"bold"}}>{partyName}</p>
+        <p>{partyDesc}</p>
+        <p>{partyDate}</p>
       </div>
       <div className="lettercontainer">
         <img src="/letter.png" style={{ width: "60%", height: "60%",maxWidth: '1024px' }}></img>
@@ -60,15 +68,29 @@ const InvitePage = () => {
     </MobileView>
 
     <BrowserView>
-      <a href="/" className="letterheader">
-        <img
-          className="headerlogo"
-          src="/pazamafont.png"
-          alt="logo"
-          width="120px"
-          height="60px"
-        ></img>
-      </a>
+    <NavBar></NavBar>
+      <p className="Main2">{partyNickname}님의 파티에 초대받으셨습니다.</p>
+      <div className="partyinfo2">
+        <p style={{color:"#FD7A99", fontSize:"large", fontWeight:"bold"}}>{partyName}</p>
+        <p>{partyDesc}</p>
+        <p>{partyDate}</p>
+      </div>
+      <div className="lettercontainer2">
+        <img src="/letter.png" style={{ width: "20%", height: "20%" }}></img>
+      </div>
+      {token !== "undefined" && token ? (
+        <>
+          <a href={`/room/${roomIdx}`} className="clicklink">
+            JOIN PARTY
+          </a>
+        </>
+      ) : (
+        <>
+          <a href={`/login/${roomIdx}`} className="clicklink">
+            로그인이 필요합니다!
+          </a>
+        </>
+      )}
     </BrowserView>
     </>
   );
