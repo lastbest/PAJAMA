c18abed3a2f92eb028b3ab4d93555143e68b5de4
Jaecheol Jeong <paneko3@gmail.com>
2022-08-17 01:09:43 +0900

    [FIX]feedDelete

--- frontend/src/components/common/Feed.js
+++ frontend/src/components/common/Feed.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Feed.module.css";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import styled from "styled-components";
 import axios from "axios";
 
@@ -15,8 +15,8 @@ const StyledInput = styled.input`
   outline: none;
   width: 100%;
   display: flex;
-  background-color: #FDF1F3;
-  font-family:"oldpicture";
+  background-color: #fdf1f3;
+  font-family: "oldpicture";
   &:focus {
     color: $oc-teal-7;
     border-bottom: 1px solid #fd7a99;
@@ -24,126 +24,167 @@ const StyledInput = styled.input`
 `;
 
 function Feed(props) {
-  let [credentials, setCredentials] = useState({  roomIdx: props.roomIdx,  description: ""});
+  let [credentials, setCredentials] = useState({
+    roomIdx: props.roomIdx,
+    description: "",
+  });
   let comment = props.comment;
-  let [write, setWrite] = useState("")
-  let pictures = props.pictures
-  const roomIdx = props.roomIdx
-  const token = sessionStorage.getItem('accessToken')
-  const partyTime = props.partyTime
-  
+  let [write, setWrite] = useState("");
+  let pictures = props.pictures;
+  const roomIdx = props.roomIdx;
+  const token = sessionStorage.getItem("accessToken");
+  const partyTime = props.partyTime;
+
   const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const [show2, setShow2] = useState(false);
   const handleClose2 = () => setShow2(false);
   const handleShow2 = () => setShow2(true);
 
   return (
     <>
-    <div className={styles.feeds}>
-      <div>
-        <button className={styles.listbtn} onClick={handleShow}><img src="/list.png" style={{"width":"20px", "height":"20px"}}></img></button>
-      </div>
-      <br />
-      <img src={pictures[0]} alt="대표사진" className={styles.picture} />
-      <br />
-      <div className={styles.comment}>
-        <div className={styles.head}>
-          <div className={styles.date}>{partyTime.substr(0,10)}</div>
-          <div>
-
-            <button className={styles.trashbtn} onClick={handleShow2}><img src="/trash.png" style={{"width":"20px", "height":"20px"}}></img></button>
-          </div>
+      <div className={styles.feeds}>
+        <div>
+          <button className={styles.listbtn} onClick={handleShow}>
+            <img
+              src="/list.png"
+              style={{ width: "20px", height: "20px" }}
+            ></img>
+          </button>
         </div>
-        <div className={styles.comment}>{comment}</div>
+        <br />
+        <img src={pictures[0]} alt="대표사진" className={styles.picture} />
+        <br />
+        <div className={styles.comment}>
+          <div className={styles.head}>
+            <div className={styles.date}>{partyTime.substr(0, 10)}</div>
+            <div>
+              <button className={styles.trashbtn} onClick={handleShow2}>
+                <img
+                  src="/trash.png"
+                  style={{ width: "20px", height: "20px" }}
+                ></img>
+              </button>
+            </div>
+          </div>
+          <div className={styles.comment}>{comment}</div>
+        </div>
       </div>
-    </div>
 
-    <Modal
-      style={{'top':'150px'}}
-      show={show}
-      onHide={handleClose}
-      backdrop="static"
-      keyboard={false}
-      className={styles.dialog}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title style={{'font-family':'star', "color":"#FD7A99"}}>PAZAMA</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className={styles.body}>
+      <Modal
+        style={{ top: "150px" }}
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        className={styles.dialog}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title style={{ "font-family": "star", color: "#FD7A99" }}>
+            PAZAMA
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={styles.body}>
+          {pictures.map((picture) => (
+            <img src={picture} alt="파티사진" className={styles.picture}></img>
+          ))}
+        </Modal.Body>
+        <Modal.Footer>
+          <StyledInput
+            autoComplete="comment"
+            name="comment"
+            placeholder=" How was the Party?"
+            onInput={(event) => {
+              setWrite(event.target.value);
+            }}
+          />
+          <Button
+            style={{
+              color: "black",
+              backgroundColor: "#FFA4BD",
+              border: "none",
+              "font-family": "oldpicture",
+              "box-shadow": "none",
+            }}
+            onClick={() => {
+              setCredentials((credentials.description = write));
+              console.log(credentials);
+              axios({
+                url: "https://i7c203.p.ssafy.io/api/mypage",
+                method: "post",
+                headers: { accessToken: token },
+                data: credentials,
+              })
+                .then((res) => {
+                  document.location.href = "/mypage";
+                  console.log(res.data);
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+          >
+            작성하기
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-        {  pictures.map((picture)=>(
-
-          <img src={picture} alt="파티사진" className={styles.picture}></img>
-        )
-        ) 
-        }
-      </Modal.Body>
-      <Modal.Footer>
-      <StyledInput
-          autoComplete="comment"
-          name="comment"
-          placeholder=" How was the Party?"
-          onInput={(event) => {
-            setWrite(event.target.value);
-          }}
-        />
-        <Button 
-        style={{'color':'black', 'backgroundColor':'#FFA4BD', 'border':'none','font-family':'oldpicture', 'box-shadow':'none' }} 
-        onClick={()=>{
-          setCredentials((credentials.description = write))
-          console.log(credentials)
-          axios({
-            url: "https://i7c203.p.ssafy.io/api/mypage",
-            method:"post",
-            headers: {accessToken : token},
-            data:credentials,
-          })
-            .then((res) => {
-              document.location.href='/mypage'
-              console.log(res.data);
-            })
-            .catch((err)=>{console.log(err)})
-
-        }}>작성하기</Button>
-      </Modal.Footer>
-    </Modal>
-
-    <Modal
-      centered
-      show={show2}
-      onHide={handleClose2}
-      backdrop="static"
-      keyboard={false}
-      className={styles.dialog}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title style={{'font-family':'star', "color":"#FD7A99"}}>PAZAMA</Modal.Title>
-      </Modal.Header>
-      <Modal.Body style={{'font-family':'oldpicture'}}>
-        해당 피드를 삭제하시겠습니까?
-      </Modal.Body>
-      <Modal.Footer>
-      <Button 
-        style={{'color':'white', 'backgroundColor':'#9D9D9D', 'border':'none','font-family':'oldpicture', 'box-shadow':'none' }} 
-        onClick={handleClose2}>취소</Button>
-        <Button 
-        style={{'color':'black', 'backgroundColor':'#FD7A99', 'border':'none','font-family':'oldpicture', 'box-shadow':'none' }}
-        onClick={()=>{
-          axios({
-            url: `https://i7c203.p.ssafy.io/api/mypage/${roomIdx}`,
-            method: 'delete',
-            headers: {accessToken : token}
-          })
-          .then((res)=>{
-            document.location.href='/mypage'
-          })
-        }}
-        >삭제하기</Button>
-      </Modal.Footer>
-    </Modal>
+      <Modal
+        centered
+        show={show2}
+        onHide={handleClose2}
+        backdrop="static"
+        keyboard={false}
+        className={styles.dialog}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title style={{ "font-family": "star", color: "#FD7A99" }}>
+            PAZAMA
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ "font-family": "oldpicture" }}>
+          해당 피드를 삭제하시겠습니까?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            style={{
+              color: "white",
+              backgroundColor: "#9D9D9D",
+              border: "none",
+              "font-family": "oldpicture",
+              "box-shadow": "none",
+            }}
+            onClick={handleClose2}
+          >
+            취소
+          </Button>
+          <Button
+            style={{
+              color: "black",
+              backgroundColor: "#FD7A99",
+              border: "none",
+              "font-family": "oldpicture",
+              "box-shadow": "none",
+            }}
+            onClick={() => {
+              axios({
+                url: `https://i7c203.p.ssafy.io/api/mypage`,
+                method: "delete",
+                headers: { accessToken: token },
+                data: {
+                  email: roomIdx,
+                },
+              }).then((res) => {
+                document.location.href = "/mypage";
+              });
+            }}
+          >
+            삭제하기
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
