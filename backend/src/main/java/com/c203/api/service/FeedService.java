013884883f0189c7a1e40e51b70c134a489864fe
김유완 <opal18@naver.com>
2022-08-16 19:33:58 +0900

    [ETC][D-00] : MAP 관련정보 수정, 피드 생성 관련 수정 MAP에 ROOMIDX암호화한 값으로 KEY변경, MAP에 시간,설명 추가, 피드 DTO EMAIL,PICTURE 제거

--- backend/src/main/java/com/c203/api/service/FeedService.java
+++ backend/src/main/java/com/c203/api/service/FeedService.java
@@ -6,7 +6,7 @@ import com.c203.api.dto.Feed.FeedShowDto;
 import java.util.Map;
 
 public interface FeedService {
-    FeedShowDto registFeed(FeedRegistDto feedRegistDto) throws Exception;
+    FeedShowDto registFeed(FeedRegistDto feedRegistDto,String email) throws Exception;
     boolean deleteFeed(String email,String roomIdx) throws Exception;
     Map showPicture(String decodeEmail) throws Exception;
 }
